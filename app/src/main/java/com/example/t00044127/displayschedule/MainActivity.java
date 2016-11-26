521a56cd42a47d969e21bceba31bb2f0ba707db3
Chirag Sehgal <T00044127@mytru.ca>
2016-11-25 16:29:52 -0800

    Initial commit

---
+++ app/src/main/java/com/example/t00044127/displayschedule/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.t00044127.displayschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    private DatabaseReference mDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        
+
+    }
+}
